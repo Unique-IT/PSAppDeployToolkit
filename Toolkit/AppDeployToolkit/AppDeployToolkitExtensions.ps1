f353cea458ad5d4721889779a6f31e2b9d712759
ms99 <markus.see@unique-it.at>
2023-01-26 20:43:28 +0100

    added PSADTWIM extension

--- Toolkit/AppDeployToolkit/AppDeployToolkitExtensions.ps1
+++ Toolkit/AppDeployToolkit/AppDeployToolkitExtensions.ps1
@@ -60,6 +60,27 @@ Param (
 ##*===============================================
 
 # <Your custom functions go here>
+function Clear-CMCache
+{
+## Initialize the CCM resource manager com object
+[__comobject]$CCMComObject = New-Object -ComObject 'UIResource.UIResourceMgr'
+## Get the CacheElementIDs to delete
+$CacheInfo = $CCMComObject.GetCacheInfo().GetCacheElements()
+## Remove cache items
+ForEach ($CacheItem in $CacheInfo) {
+
+    try
+    {
+    $null = $CCMComObject.GetCacheInfo().DeleteCacheElement([string]$($CacheItem.CacheElementID))
+    }
+    catch
+    {}
+}
+
+
+}
+
+. $ScriptRoot\PSADTWIM.ps1
 
 ##*===============================================
 ##* END FUNCTION LISTINGS
